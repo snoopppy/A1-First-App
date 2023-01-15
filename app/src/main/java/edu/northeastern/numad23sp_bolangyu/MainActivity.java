1a4ccf372cdb36d6060cb29d0d39648a9049d5d1
snoop <496150107@qq.com>
2023-01-14 19:42:56 -0800

    Add toast into the app

--- app/src/main/java/edu/northeastern/numad23sp_bolangyu/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_bolangyu/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Name: Bolang Yu",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"Email: yu.bol@northeastern.edu",Toast.LENGTH_LONG).show();
                 openAboutMe();
             }
         });
